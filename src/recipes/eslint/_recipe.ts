ebaf26b22a00dc0df9e57cbc039af141339b1314
Pridestalkerr <danartenie158@gmail.com>
2023-04-20 20:00:53 +0300

    eslint recipe

--- src/recipes/eslint/_recipe.ts
+++ src/recipes/eslint/_recipe.ts
@@ -1,11 +1,37 @@
 import { $ } from "execa";
+import { ProjectOptions } from "~/installer.js";
 
-export default {
+import { type Recipe } from "~/recipes/index.js";
+
+const recipe: Recipe = {
   packageJson: {
     scripts: {},
     dependencies: {},
-    devDependencies: {},
+    devDependencies: {
+      eslint: "latest",
+      "eslint-config-next": "latest",
+      "@typescript-eslint/parser": "latest",
+      "@typescript-eslint/eslint-plugin": "latest",
+    },
   },
-  arch: {},
-  steps: [],
+  arch: {
+    ".eslintrc.js": "replaces",
+  },
+  steps: [
+    {
+      id: "setup-eslint",
+      label: "Creating Next.js app...",
+      description: "Create a Next.js app with the default template",
+      run: async ({
+        projectDir,
+        language,
+        // packageManager,
+        importAlias,
+      }: ProjectOptions) => {
+        await $`npx create-next-app@latest ${projectDir} --${language} --no-tailwind --no-eslint --src-dir --no-experimental-app --import-alias ${importAlias}`;
+      },
+    },
+  ],
 };
+
+export default recipe;
